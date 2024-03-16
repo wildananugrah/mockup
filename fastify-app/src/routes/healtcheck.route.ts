59b60a725fdb89cede850a419d632774631500d9
wildananugrah <wildananugrah@gmail.com>
2024-03-16 22:44:05 +0700

    fastify ts connect to own modules

--- fastify-app/src/routes/healtcheck.route.ts
+++ fastify-app/src/routes/healtcheck.route.ts
@@ -1,9 +1,12 @@
+import { HealthCheck } from "user-management-db/HealthCheck";
+
 const routes = async (app: any, options: any) => {
   app.route({
     method: "GET",
     url: "/_/healthcheck",
     handler: async (req: any, res: any) => {
-      return res.status(200).send({ app: true });
+      const db = await new HealthCheck(await app.dbPool.connect()).test();
+      return res.status(200).send({ app: true, db });
     },
   });
 };
--- fastify-app/src/routes/user.route.ts
+++ fastify-app/src/routes/user.route.ts
@@ -1,16 +1,36 @@
+import { JWTService } from "jwt-service";
+import { UserService } from "user-management-db/UserService";
+import { certificate, privateKey } from "../configs/jwt.config";
+
 const routes = async (app: any, options: any) => {
   app.route({
     method: "POST",
     url: "/api/users/register",
     handler: async (req: any, res: any) => {
-      return res.status(200).json({ message: "implement register api" });
+      const userService = new UserService(await app.dbPool.connect());
+      const { username, password } = req.body;
+      const user = await userService.register({ username, password });
+      const jwtService = new JWTService({
+        privateKey: privateKey,
+        certificate: certificate,
+      });
+      const token = await jwtService.create(user, 3600);
+      return res.status(200).send(token);
     },
   });
   app.route({
     method: "POST",
     url: "/api/users/login",
     handler: async (req: any, res: any) => {
-      return res.status(200).json({ message: "implement login api" });
+      const userService = new UserService(await app.dbPool.connect());
+      const { username, password } = req.body;
+      const user = await userService.login({ username, password });
+      const jwtService = new JWTService({
+        privateKey: privateKey,
+        certificate: certificate,
+      });
+      const token = await jwtService.create(user, 3600);
+      return res.status(200).send(token);
     },
   });
 };
